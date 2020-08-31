4ea2f24cd9bd3e31d518a2ce02a25adc0dd307d9
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-31 12:52:20 +0200

    add comment

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ToDoInput from "./components/ToDoInput";
 import ToDoList from "./components/ToDoList";
 
+//hi there
 class App extends Component {
   render() {
     return (
