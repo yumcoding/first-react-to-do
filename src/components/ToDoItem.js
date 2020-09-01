aedf44ce1eb57f9841f01f9d75899dd8f07d7201
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-09-01 11:29:29 +0200

    update TodoItem

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 
-class ToDoItem extends Component {
+class TodoItem extends Component {
   render() {
-    return <div>Hello from ToDoItem</div>;
+    const { title, handleEdit, handleRemove } = this.props;
+    return (
+      <div className="todo-item">
+        <h5>{title}</h5>
+        <span onClick={handleEdit}>
+          <i className="fas fa-pen" />
+        </span>
+        <span onClick={handleRemove}>
+          <i className="fas fa-trash" />
+        </span>
+      </div>
+    );
   }
 }
 
-export default ToDoItem;
+export default TodoItem;
